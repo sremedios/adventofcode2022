3328d4cbf00255c3b9611544812a50d583254bec
Samuel W. Remedios <sam.remedios@gmail.com>
2022-12-01 17:42:22 -0500

    day1

---
+++ src/day_01.rs
@@ -0,0 +1,67 @@
+use std::cmp::Ordering;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read};
+
+fn count_calories(calories_list: Vec<String>) -> Vec<u32> {
+    calories_list
+        .group_by(|a, b| a.parse::<u32>().is_ok() && b.parse::<u32>().is_ok())
+        .filter(|&x| x != ["".to_string()])
+        .map(|v| v.iter().map(|x| x.parse::<u32>().unwrap()).sum::<u32>())
+        .collect()
+}
+
+fn top_k_calories(calories_list: Vec<String>, k: usize) -> u32 {
+    let mut tmp = count_calories(calories_list);
+    tmp.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal));
+    tmp.iter().rev().take(k).sum()
+}
+
+pub fn part_1(filename: &str) -> Result<String, io::Error> {
+    let f = File::open(filename)?;
+
+    let data: Vec<String> = BufReader::new(&f)
+        .by_ref()
+        .lines()
+        .map(|l| l.unwrap().to_string())
+        .collect();
+
+    Ok(top_k_calories(data, 1).to_string())
+}
+
+pub fn part_2(filename: &str) -> Result<String, io::Error> {
+    let f = File::open(filename)?;
+
+    let data: Vec<String> = BufReader::new(&f)
+        .by_ref()
+        .lines()
+        .map(|l| l.unwrap().to_string())
+        .collect();
+
+    Ok(top_k_calories(data, 3).to_string())
+}
+
+#[test]
+fn test_part_1() {
+    let data: Vec<String> = vec![
+        "1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "",
+        "10000",
+    ]
+    .iter()
+    .map(|s| s.to_string())
+    .collect();
+
+    assert_eq!(top_k_calories(data, 1), 24000);
+}
+
+#[test]
+fn test_part_2() {
+    let data: Vec<String> = vec![
+        "1000", "2000", "3000", "", "4000", "", "5000", "6000", "", "7000", "8000", "9000", "",
+        "10000",
+    ]
+    .iter()
+    .map(|s| s.to_string())
+    .collect();
+
+    assert_eq!(top_k_calories(data, 3), 45000);
+}
---
+++ src/main.rs
@@ -0,0 +1,51 @@
+#![feature(slice_group_by)]
+
+use std::env;
+use std::io;
+use std::path::PathBuf;
+
+mod day_01;
+
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env::args().collect();
+
+    // Double digit formatting
+    let day = if args[1].chars().count() == 1 {
+        format!("0{}", args[1])
+    } else {
+        args[1].to_string()
+    };
+
+    let input_filename: PathBuf = ["resources", format!("{}{}{}", "day_", day, ".txt").as_ref()]
+        .iter()
+        .collect();
+
+    let input_filename = match input_filename.to_str() {
+        Some(f) => f,
+        None => "Error parsing filename.",
+    };
+
+    let (part_1_answer, part_2_answer) = match day.as_ref() {
+        "01" => (
+            day_01::part_1(input_filename)?,
+            day_01::part_2(input_filename)?,
+        ),
+        _ => ("Invalid argument".to_owned(), "Invalid arg".to_owned()),
+    };
+    /*
+    let (part_1_answer, part_2_answer) = match day.as_ref() {
+        "01" => (
+            day_01::part_1(input_filename)?,
+            day_01::part_2(input_filename)?,
+        ),
+        _ => ("Invalid argument".to_owned(), "Invalid arg".to_owned()),
+    };
+    */
+
+    println!(
+        "===== Day {} =====\nPart 1: {}\nPart 2: {}",
+        args[1], part_1_answer, part_2_answer
+    );
+
+    Ok(())
+}
